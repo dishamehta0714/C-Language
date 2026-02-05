70cd0b99caeea427769ea32dd0b33ced958fb533
DISHA MEHTA <disha.mehta1410@gmail.com>
2026-02-05 16:37:13 +0530

    Add files via upload

---
+++ 1_intro/1_basic_structure.c
@@ -0,0 +1,14 @@
+
+
+#include<stdio.h>
+
+
+int main(){
+
+
+    printf("Welcome to C language");
+
+    printf("\nHello World");
+
+    return 0;
+}
---
+++ 1_intro/2_escape.c
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+
+
+int main(){
+
+
+    printf("Hellow!!!\n");
+
+
+    printf("good\nmorning");
+
+    printf("\nhow\tare\tyou????");
+
+    printf("\nwelcom to the world\\");
+
+    printf("\nwhat are you doing???\"");
+
+        return 0;
+}
