b65fa9b788a689d2d2580fbdcfd1bd919eb972cb
DISHA MEHTA <disha.mehta1410@gmail.com>
2026-02-06 16:40:22 +0530

    Add files via upload

---
+++ 2_datatypes/2_datatype.c
@@ -0,0 +1,19 @@
+
+
+#include<stdio.h>
+
+
+int main(){
+
+    int num = 10;
+
+    float num2 = 20.67;
+
+    char singlecharacter = "d";
+
+    char mystring[] = "hello";
+
+    char* string = 'p';
+
+    
+}
---
+++ 2_datatypes/3_formatspacifire.c
@@ -0,0 +1,28 @@
+
+
+#include<stdio.h>
+
+int main(){
+
+    int num = 1431;
+
+    printf("%d\n" ,num);
+
+    
+    float num2 = 80.89;
+
+    printf("%f\n",num2);
+
+    char singlecharacter = 'p';
+
+    printf("%c\n",singlecharacter);
+
+    char mystring[] = "hello";
+
+    printf("%s\n",mystring);
+
+    char* string = "d";
+
+    printf("%s\n",string);
+
+}
