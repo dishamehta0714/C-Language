4f01ba7298cc606f962b49423eec0f8ab8b43b4d
DISHA MEHTA <disha.mehta1410@gmail.com>
2026-02-24 17:01:01 +0530

    Add files via upload

---
+++ 10_loop/1_while.c
@@ -0,0 +1,18 @@
+
+
+#include<stdio.h>
+
+int main(){
+
+
+    int num = 10;
+    int i =0;
+
+    while (i < 10)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    
+    return 0;
+}
---
+++ 10_loop/2_while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum = 0;
+    int i = 0;
+
+    while (i < 10)
+    {
+        sum += i;
+        i++;
+    }
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
