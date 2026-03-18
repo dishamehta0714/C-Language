b4a52963ffa0240b14c2be7e0f5c33a45e82e32d
DISHA MEHTA <disha.mehta1410@gmail.com>
2026-03-18 13:01:13 +0530

    Add files via upload

---
+++ 20_string/1_char.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char lett = 'P';
+
+    printf("%c", lett);
+    printf("\n%d\n", lett);
+
+    char letter = 'D';
+
+    printf("%c", letter);
+    printf("\n%d\n", letter);
+
+    return 0;
+}
