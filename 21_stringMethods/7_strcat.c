35c6ba73e316c97555084c04b79f24adb18cd1f0
DISHA MEHTA <disha.mehta1410@gmail.com>
2026-03-20 20:39:41 +0530

    Add files via upload

---
+++ 21_stringMethods/7_strcat.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+
+    char str1[]="hello";
+    char str2[]="goodMorning";
+
+    strcat(str1,str2);
+
+    printf("concating :%s",str1);
+
+
+    return 0;
+}
